61def23c14b26e6e7d93fb0bb01f932a91159bf7
1Sting1 <146950386+1Sting1@users.noreply.github.com>
2024-03-13 11:22:25 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -3,61 +3,82 @@ from Storage import Storage
 from RFIDReader import RFIDReader
 from ButtonReader import ButtonReader
 from Clean import ProgramExitManager
+from SessionLoader import SessionMonitor
+import datetime
 import time
+
 import RPi.GPIO as GPIO
 
-
+monitor = SessionMonitor()
 storage = Storage()
 recipt = Recipt()
-exit_manager = ProgramExitManager
+exit_manager = ProgramExitManager()
+exit_manager.recipt = recipt
 
-try:
-    exit_manager.pin_extra_clean()
-    while True:
-        while True:
-            rfid_reader = RFIDReader()
-            rfid = rfid_reader.start_reading()
-            rfid_exit = exit_manager.clean_rfid()
-            if rfid:
-                recipt.start_time = time.time()
+exit_manager.clean_all
+exit_manager.pin_extra_clean
+exit_manager.clean
 
-                """ 1. ID чека к которому прикрепить заказ
-                2. Номер выбранной бутылки
-                3. Объем, который необходимо налить"""
+def main():
 
-                """ После завершения считывания RFID метки начинаем считывание кнопок """
-                bot = recipt.get_bottle_number
-                print(bot)
+    try:
+        while monitor.monitor_session() == True:
+            try:
+                while True:
+                    rfid_reader = RFIDReader()
+                    exit_manager.rfid = rfid_reader
 
-                button_reader = ButtonReader(storage.led_pin(bot),
-                                         storage.button_pin(bot))
-                button_reader.setup()
-                button_reader.wait_for_events()
-                button_exit = exit_manager.clean_button()
+                    rfid = rfid_reader.start_reading()
+                    if rfid:
+                        recipt.start_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-                while 1:
-                    time.sleep(1)
-                    """Ждем конца работы насосов"""
-                    if storage.result == 1:
-                        time.sleep(3)
-                    break
+                        """ 1. ID чека к которому прикрепить заказ
+                        2. Номер выбранной бутылки
+                        3. Объем, который необходимо налить"""
 
-                """ Формируем модель чека и отправляем данные на сервер """
-                recipt.end_time = time.time()
-                recipt.total_create
-                recipt_clean = exit_manager.clean_recipt()
-                time.sleep(2)
+                        """ После завершения считывания RFID метки начинаем считывание кнопок """
+                        bot = recipt.get_bottle_number
+                        print(bot)
+
+                        button_reader = ButtonReader(storage.led_pin(bot), storage.button_pin(bot))
+                        exit_manager.Button = button_reader
+                        button_reader.setup()
+                        button_reader.wait_for_events()
+
+                        while 1:
+                            time.sleep(1)
+                            """Ждем конца работы насосов"""
+                            if storage.result == True:
+                                time.sleep(3)
+                                break
+
+                        """ Формируем модель чека и отправляем данные на сервер """
+                        recipt.end_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                        recipt.total_create
+                        time.sleep(2)
+
+                    storage.set_result = False
+                    exit_manager.clean
+
+            #print("GOOD")
+
+            except:
+                print("Ошибка в исполнении основной программы:")
+
+    finally:
+        exit_manager.clean_all
+        exit_manager.pin_extra_clean
+        exit_manager.clean
 
 
-            storage.set_result = False
+while monitor.monitor_session() == False:
+    time.sleep(5)
+    main()
 
-except Exception as fail:
-    print(fail)
-    exit_manager.pin_extra_clean()
 
-finally:
-    exit_manager.clian()
-        #print("GOOD")
+
+
+
 
 
 
